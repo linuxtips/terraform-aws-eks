ea616edc72fea689228240c50430f6d91aaa42bc
Jeferson Fernando <jfnredes@gmail.com>
2024-10-24 19:50:39 +0200

    adicionando a nossa primeira versao

---
+++ terraform-eks-module/route_tables.tf
@@ -0,0 +1,37 @@
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = merge(var.tags, { Name = "${var.name}-${var.environment}-public-rt" })
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = merge(var.tags, { Name = "${var.name}-${var.environment}-private-rt" })
+}
+
+# Public Route Table Associations
+resource "aws_route_table_association" "public_rt_association" {
+  count          = length(aws_subnet.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Private Route Table Associations
+resource "aws_route_table_association" "private_rt_association" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
