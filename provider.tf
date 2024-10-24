1d33beec1b7205ac7617cdd6f9cb087462ede715
Gomex <rgomes@dnsfilter.com>
2024-10-24 15:33:33 -0300

    fix: mudança sugerida pelo tflint

--- provider.tf
+++ provider.tf
@@ -3,10 +3,15 @@ provider "aws" {
 }
 
 terraform {
+  required_version = "1.5.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -119,5 +119,5 @@ variable "project" {
 variable "node_ami_type" {
   description = "AMI ID for worker nodes"
   type        = string
-  default     = "ami-XXXXXXXXXXXXXXXXX"
+  default     = "AL2_x86_64"
 }
